0a071393be69440da53f61f1a529f93f6597fd5b
mcdestefano <124924561+mcdestefano@users.noreply.github.com>
2023-03-16 16:34:52 -0400

    Fix populateDisplay and ensure it doesn't break on reasonable inputs

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let display = document.querySelector('.display');
-let displayValue = +display.textContent;
-let operator;
-let prev = 0;
+let displayValue = display.textContent;
+let operator = null;
+let prev = null;
+let starting = true;
 
 const numButtons = document.querySelectorAll('.num-button');
 numButtons.forEach((button) => {
@@ -21,7 +22,7 @@ opButtons.forEach((button) => {
 
 
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a,b) {
@@ -54,26 +55,169 @@ function operate(op, a, b){
     }
 }
 
+// function populateDisplayNum(num) {
+//     if (displayValue === 0) {// instead of concat, replace
+//         displayValue = num;
+//     } else {
+//         displayValue = displayValue + num;
+//     }
+//     display.textContent = displayValue;
+// }
+
+// function populateDisplayOp(op) {
+//     if (operator) {
+//         runOperation(operator);
+//     }
+//     operator = label;
+//     display.textConent = displayValue;
+// }
+
+// function populateDisplay(label) {
+//     if (typeof label === 'number') { // this doesn't work!!!
+//         if (displayValue === 0) { // instead of concat, replace
+//             displayValue = label;
+//         } else {
+//             displayValue = displayValue + label; // want this to be string concat
+//         }
+//     } else {
+//         if (operator) {
+//             runOperation(operator);
+//             operator = label;
+//         } else { // initial press of operation
+//             operator = label;
+//         }
+//     }
+//     display.textContent = displayValue;
+// }
+
 function populateDisplay(label) {
-    if(typeof label === 'number'){ // this doesn't work!!!
-        if(displayValue === 0){ // instead of concat, replace
-            displayValue = label;
-        } else {
-        displayValue = displayValue + label; // want this to be string concat
+    if(label === "Clear"){ // resets if select clear
+        displayValue = '0';
+        prev = null;
+        operator = null;
+        display.textContent = displayValue;
+        return;
+    }
+    if(isNaN(displayValue)){
+        return;
+    }
+    if(prev != null){
+        if(!isNaN(label)){// if typing number
+            // continue building number
+            displayValue += label;
+        } else { // if typing operator
+            // do previous operation and store operator
+            displayValue = operate(operator, prev, displayValue);
+            if(isNaN(displayValue)){
+                displayValue = "No dividing by 0! Clear and try again.";
+                display.textContent = displayValue;
+                return;
+            }
+            prev = null;
+            operator = label;
         }
-    } else {
-        if(operator){
-            runOperation(operator);
-            operator = label;
-        } else { // initial press of operation
-            operator = label;
+    } else {// prev is null, i.e. at start or awaiting next term in operation
+        if(operator){// there is an operator, so we are inputing the next term
+            if (operator === '='){ // special case with equals
+                if(isNaN(label)){
+                    operator = label; // moves on smoothly to next operation
+                } else { // restarts process as if restarted calculator
+                    displayValue = label;
+                    operator = null;
+                }
+            } else {
+                if(label === "="){
+                    displayValue = "Error. Clear and try again.";
+                    display.textContent = displayValue;
+                    return;
+                } else {
+                    prev = displayValue;
+                    displayValue = label;
+                }
+            }
+        } else {// operator is null, i.e. at start (or inputing number still)
+            if (isNaN(label)) { // if you input your first operator
+                if(starting){
+                    return; // do nothing if put operator with no values yet
+                }
+                operator = label;
+            } else if(displayValue === '0'){
+                displayValue = label;
+                starting = false;
+            } else {
+                displayValue += label; // !!! this will mess up if you start with an operator
+                starting = false;
+            }
         }
     }
-    display.textContent = displayValue;    
+    displayValue = Math.round(displayValue*1000)/1000;
+    display.textContent = displayValue;
 }
 
-function runOperation(op){ //updates values of displayValue and operator by running operation
-    const newValue = operate(op, prev, displayValue);
-    prev = displayValue;
-    displayValue = newValue;
-}
+/*
+start
+display 0
+op null
+prev null
+
+type 5
+display 5
+op null
+prev null
+
+type *
+dispaly 5
+op *
+prev null
+
+type 1
+display 1
+op *
+prev 5
+
+type 0
+display 10
+op *
+prev 5
+
+type +
+dispaly 50
+op +
+prev  null
+
+type 7
+display 7
+op +
+prev 50
+
+type =
+display 57
+op =
+prev null
+
+type 3
+display 3
+op null
+prev null
+
+type 0
+display 30
+op null
+prev null
+
+type +
+display 30
+op +
+prev null
+
+type 7
+display 7
+op +
+prev 30
+
+type =
+display 37
+op =
+prev null
+
+*/
